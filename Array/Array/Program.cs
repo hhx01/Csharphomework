a14d334a3d4c8be7d5a70006ee2413131c5224d8
hhxlyy <2018302110240@whu.edu.cn>
2020-02-29 16:05:48 +0800

    编程求一个整数数组的最大值、最小值、平均值和所有数组元素的和。

---
+++ Array/Array/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        public void MaxMin(int[] a, out int max, out int min)
+        {
+            int i = 1;
+            max = min = a[0];
+            while (i < a.Length)
+            {
+                if (max <= a[i])
+                {
+                    max = a[i];
+                }
+                if (min >= a[i])
+                {
+                    min = a[i];
+                }
+                i++;
+            }
+        }
+        public void Calculator(int[] a, out double sum, out double average)
+        {
+            sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum = sum + a[i];
+            }
+            average = sum / a.Length;
+        }
+        static void Main(string[] args)
+        {
+            Program cal = new Program();
+            int[] arrayNum = { 24, 224, 3, 43, 4346, 2, 42, 2, 45, 224, 222, 124 };
+            int max, min;
+            double average, sum;
+            cal.MaxMin(arrayNum, out max, out min);
+            cal.Calculator(arrayNum, out sum, out average);
+            Console.WriteLine("最大值：{0};最小值：{1};和：{2};平均值：{3}", max, min, sum, average);
+            Console.WriteLine("按任意键退出程序...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IsPrime/IsPrime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace isPrime
+{
+    class Program
+    {
+        public void IsPrime(int a, out List<int> list)          //输入目标数字，传出结果数组
+        {
+            list = new List<int>();
+            int nCount = 0;
+            list.Add(2);
+            //产生初始数组并去掉2的倍数
+            for (int i = 3; i < a; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+            //循环去掉所有非质数
+            while (list[nCount] * list[nCount] < list[list.Count - 1])
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] % list[nCount] == 0)
+                    {
+                        list.Remove(list[i]);
+                    }
+                }
+                nCount++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入目标整数：");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Program Ai = new Program();
+            List<int> list = new List<int>();
+            Ai.IsPrime(num, out list);
+            Console.WriteLine("{0}中的所有质数为：", num);
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine("按任意键继续。。。");
+            Console.ReadLine();
+        }
+    }
+}
