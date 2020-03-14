7710851e57b0cd93705cef538bf02734609e4ec7
hhxlyy <2018302110240@whu.edu.cn>
2020-03-14 19:16:50 +0800

    1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。
    2、使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台显示提示信息。

---
+++ GenericList/GenericList/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> f)
+        {
+            for (Node<T> t = this.head; t != null; t = t.Next)
+            {
+                f(t.Data); 
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            intlist.ForEach(t => Console.WriteLine(t));
+            int sum = 0;
+            intlist.ForEach(x=> { sum += x; });
+            Console.WriteLine(sum);
+            int max = 0,min = 0;
+            intlist.ForEach(x => max = Math.Max(max,x));
+            intlist.ForEach(x =>min = Math.Min(min,x));
+            Console.WriteLine("最大值：{0},最小值：{1}",max,min);
+
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            for (Node<string> node = strList.Head;
+                    node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
